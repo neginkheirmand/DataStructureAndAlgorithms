7ce4bc45808aadd4424ab5b3edb4dbb9183f8447
neginkheirmand <neginkheirmand@gmail.com>
2020-10-16 03:26:03 +0330

    added part2 of the Q4

---
+++ Q4-Part2.py
@@ -0,0 +1,20 @@
+def main():
+    numElements = int(input("please enter the number of elements in the list\n"))
+    while numElements<=0:
+        numElements = int(input("please enter the number of elements in the list\n"))
+    listOfElements = input("enter the elements of the list in the next row(should be sorted from min to max)\n").split()
+    for i in range(0, numElements):
+        listOfElements[i] = int(listOfElements[i])
+
+    print("this scrypt should find numbers a and b in the sorted list specified that can satisfy the condition: a+b = k")
+    #finding 
+    k = int(input("please enter k: "))
+
+    for i in range(0, numElements):
+        for j in range(i+1, numElements):
+            if listOfElements[i]+listOfElements[j]==k :
+                print("list[{}] = {}\nlist[{}] = {}\n{}+{}={}".format(i,listOfElements[i],j, listOfElements[j], listOfElements[i], listOfElements[j], k))
+                return
+    print("none of the elements in the list, can satisfy the condition specified")
+    
+main()
