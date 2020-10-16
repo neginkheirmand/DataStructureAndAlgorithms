1b2d6a71bc248a7a7f0b98e5e2b55232192db1df
neginkheirmand <neginkheirmand@gmail.com>
2020-10-16 21:20:15 +0330

    working on the second part

--- Q4-Part2.py
+++ Q4-Part2.py
@@ -1,3 +1,28 @@
+def positionFinder(eList, num, start, end):
+    if end-start==1:
+        if eList[start] == num:
+            return start
+        elif eList[end] == num:
+            return end
+        else:
+            return -1
+    if (start+end)%2==1:
+        if num>=eList[ 1 + int( (start+end)/2 ) ]:
+            positionFinder(eList, num, 1 + int( (start+end)/2 ), end)
+        else:
+            positionFinder(eList, num, start, 1 + int( (start+end)/2 ))
+    else:
+        if num>=eList[ int( (start+end)/2 ) ]:
+            positionFinder(eList, num, int( (start+end)/2 ), end)
+        else:
+            positionFinder(eList, num, start, int( (start+end)/2 ))
+        
+
+    
+        
+
+
+
 def main():
     numElements = int(input("please enter the number of elements in the list\n"))
     while numElements<=0:
@@ -7,14 +32,14 @@ def main():
         listOfElements[i] = int(listOfElements[i])
 
     print("this scrypt should find numbers a and b in the sorted list specified that can satisfy the condition: a+b = k")
-    #finding 
     k = int(input("please enter k: "))
 
-    for i in range(0, numElements):
-        for j in range(i+1, numElements):
-            if listOfElements[i]+listOfElements[j]==k :
-                print("list[{}] = {}\nlist[{}] = {}\n{}+{}={}".format(i,listOfElements[i],j, listOfElements[j], listOfElements[i], listOfElements[j], k))
-                return
-    print("none of the elements in the list, can satisfy the condition specified")
+    #find position of k/2
+    # for i in range(0, numElements):
+    #     for j in range(i+1, numElements):
+    #         if listOfElements[i]+listOfElements[j]==k :
+    #             print("list[{}] = {}\nlist[{}] = {}\n{}+{}={}".format(i,listOfElements[i],j, listOfElements[j], listOfElements[i], listOfElements[j], k))
+    #             return
+    # print("none of the elements in the list, can satisfy the condition specified")
     
-main()
+# main()
