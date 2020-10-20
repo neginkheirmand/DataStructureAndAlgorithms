43906ab56c1dbf6118ed609ce4efd3d68cef5789
neginkheirmand <neginkheirmand@gmail.com>
2020-10-20 13:44:35 +0330

    fixed bugs

--- Q4-Part2.py
+++ Q4-Part2.py
@@ -1,12 +1,12 @@
 def positionFinder(eList, num, start, end):
-    print(eList[start:end+1])
+    # print(eList[start:end+1])
     if end-start==1:
         if eList[start] == num:
             return start
         elif eList[end] == num:
             return end
         else:
-            return -1
+            return None
     if (start+end)%2==1:
         if num>=eList[ 1 + int( (start+end)/2 ) ]:
             return positionFinder(eList, num, 1 + int( (start+end)/2 ), end)
@@ -17,28 +17,6 @@ def positionFinder(eList, num, start, end):
             return positionFinder(eList, num, int( (start+end)/2 ), end)
         else:
             return positionFinder(eList, num, start, int( (start+end)/2 ))
-        
-
-def closestPositionFinder(eList, num, start, end):
-    print(eList[start:end+1])
-    if end-start==1:
-        print("yess end={} start={}".format(end, start))
-        if eList[start] == num:
-            return start
-        else:
-            return end
-    if (start+end)%2==1:
-        if num>=eList[ 1 + int( (start+end)/2 ) ]:
-            return closestPositionFinder(eList, num, 1 + int( (start+end)/2 ), end)
-        else:
-            return closestPositionFinder(eList, num, start, 1 + int( (start+end)/2 ))
-    else:
-        if num>=eList[ int( (start+end)/2 ) ]:
-            return closestPositionFinder(eList, num, int( (start+end)/2 ), end)
-        else:
-            return closestPositionFinder(eList, num, start, int( (start+end)/2 ))
-        
-
 
 
 def main():
@@ -51,11 +29,14 @@ def main():
 
     print("this scrypt should find numbers a and b in the sorted list specified that can satisfy the condition: a+b = k")
     k = int(input("please enter k: "))
-    # print(closestPositionFinder(listOfElements, k, 0, len(listOfElements)-1))
-    middle = closestPositionFinder(listOfElements, int(k/2), 0, len(listOfElements))
-    for i in range(0, middle):
-        b = k- listOfElements[i]
-        if positionFinder(listOfElements, k, middle, len(listOfElements)-1)!=-1:
+    for i in range(0, len(listOfElements) - 1 ):
+        print(listOfElements[i])
+        b = k - listOfElements[i]
+        positionFound = positionFinder(listOfElements, b, 0, len(listOfElements)-1)
+        # print('\033[93m', positionFound, '\033[0m')
+        if positionFound!=None:
             print("this list satisfies the condition")
-    print("this list does not satisfie the condition")
+            print("{} + {} = {}".format(listOfElements[i], b, k))
+            return
+    print("this list does not satisfy the condition")
 main()
