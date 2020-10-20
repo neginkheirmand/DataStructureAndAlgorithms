d940aa2a48f289488d53a13da0b18a124004b9d6
neginkheirmand <neginkheirmand@gmail.com>
2020-10-20 17:13:45 +0330

    iterative implementation completed

--- iterativeBinarySearch.py
+++ iterativeBinarySearch.py
@@ -1,33 +1,4 @@
 def main():
-    eList =[1, 2, 3, 4, 5, 6, 7, 8, 9]
-    n = 9
-    k = 9
-    index = None
-    start = 0
-    end = n-1
-    mid = int((n-1)/2)
-    while mid<=end:
-        print(eList[start: end+1])
-        print(start, end)
-        if end-start<=1:
-            if eList[start]==k:
-                index = start
-            elif eList[end] == k:
-                index = end
-            break                
-        mid = int((end+start)/2)
-        print('\033[93m', mid, '\033[0m')
-        if eList[mid]>k:
-            end = mid
-        else:
-            start = mid
-
-        print("----\n")
-
-    print(index)
-
-
-def main():
     n = int(input("size of the list?\n"))
     while n<=0:
         n = int(input("size of the list?\n"))
@@ -46,8 +17,8 @@ def main():
         end = n-1
         mid = int((n-1)/2)
         while mid<=end:
-            print(eList[start: end+1])
-            print(start, end)
+            # print(eList[start: end+1])
+            # print(start, end)
             if end-start<=1:
                 if eList[start]==b:
                     index = start
@@ -55,12 +26,10 @@ def main():
                     index = end
                 break                
             mid = int((end+start)/2)
-            if eList[mid]>k:
+            if eList[mid]>b:
                 end = mid
             else:
                 start = mid
-
-
         # print('\033[93m', positionFound, '\033[0m')
         if index!=None:
             print("this list satisfies the condition")
