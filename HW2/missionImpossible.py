b697365b7acebb6d7bb4770a711f6591eca087af
neginkheirmand <neginkheirmand@gmail.com>
2020-11-02 03:03:09 +0330

    third try : 100/100

--- HW2/missionImpossible.py
+++ HW2/missionImpossible.py
@@ -1,12 +1,27 @@
 #if Ur asking about the file name : i just wanted to make it look fun
-inputString = input().split()
-cargo = []
-for i in range(0, len(inputString)):
-        cargo.append(int(inputString[i]))
-cargo.insert(0, cargo.pop())
-insertionIndex = 2
-while insertionIndex<len(cargo)-1:
-        cargo.insert(insertionIndex, cargo.pop())
-        insertionIndex+=2
-for i in range(0, len(cargo)):
-        print(cargo[i], end =" ")  
+def main():
+        inputString = input().split()
+        cargo = []
+        newCargo = []
+        for i in range(0, len(inputString)):
+                cargo.append(int(inputString[i]))
+        if len(cargo)==1:
+                print(cargo[0])
+                return
+        if len(cargo)==0:
+                return
+        newCargo.insert(0, cargo.pop())
+        newCargo.append(cargo[0])
+        i=1
+        while i<len(cargo):
+                # print(cargo)
+                # print(newCargo)
+                newCargo.append(cargo.pop())
+                if i<len(cargo):
+                        newCargo.append(cargo[i])
+                i+=1
+
+        for i in range(0, len(newCargo)):
+                print(newCargo[i], end =" ")  
+
+main()
