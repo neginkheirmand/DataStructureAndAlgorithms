41a425f5ea2739bc1f7cfed667f61288730c81f7
neginkheirmand <neginkheirmand@gmail.com>
2020-11-02 20:06:43 +0330

    class added and working but has some bugs:(

--- HW2/NotStackItIsPOSHTE.py
+++ HW2/NotStackItIsPOSHTE.py
@@ -20,7 +20,8 @@ class LinkedList:
 		((self.head).after).before = self.head
 		self.head = (self.head).after
 		if self.sizeList%2==1:
-			self.middleNode = (self.middleNode).after	
+			self.middleNode = (self.middleNode).after
+
 	def pop(self):
 		if self.sizeList==0:
 			return
@@ -28,21 +29,52 @@ class LinkedList:
 		if self.sizeList==0:
 			self.head = None
 			self.middleNode = None
-		
-		
+			return
+		self.head = (self.head).before
+		(self.head).after = None
+		if self.sizeList%2==0:
+			self.middleNode = (self.middleNode).before
 
+	def printPoshte(self):
+		pointer = self.head
+		while pointer!=None:
+			print(pointer.data, end=' ')
+			pointer = pointer.before
+		print()
+		
+	def findMiddleOfPoshte(self):
+		if self.sizeList!=0:
+			print(self.middleNode.data)
+			return
+		print(-1)
+
+	def removeMiddleOfPoshte(self):
+		if self.sizeList==0:
+			return
+		self.sizeList-=1
+		if self.sizeList==0:
+			self.head=None
+			self.middleNode=None
+			return
+		((self.middleNode).before).after=(self.middleNode).after
+		((self.middleNode).after).before = (self.middleNode).before
+		if self.sizeList%2==0:
+			self.middleNode = (self.middleNode).before
+		else:
+			self.middleNode=(self.middleNode).after
+		
 if __name__=='__main__': 
 	poshte = LinkedList()
-        inputForm = input().split()
-        while inputForm[0]!="finish":
-                if inputForm[0]=="push":
-                        poshte.pushToPoshte(int(inputForm[1]))
-                elif inputForm[0]=="pop":
-                        pop()
-                elif inputForm[0]=="print":
-                        printPoshte()
-                elif inputForm[0]=="findMiddle":
-                        findMiddleOfPoshte()
-                elif inputForm[0]=="removeMiddle":
-                        removeMiddleOfPoshte()
-                inputForm = input().split()
+	inputForm = input().split()
+	while inputForm[0]!="finish":
+			if inputForm[0]=="push":
+					poshte.pushToPoshte(int(inputForm[1]))
+			elif inputForm[0]=="pop":
+					poshte.pop()
+			elif inputForm[0]=="print":
+					poshte.printPoshte()
+			elif inputForm[0]=="findMiddle":
+					poshte.findMiddleOfPoshte()
+			elif inputForm[0]=="removeMiddle":
+					poshte.removeMiddleOfPoshte()
+			inputForm = input().split()
