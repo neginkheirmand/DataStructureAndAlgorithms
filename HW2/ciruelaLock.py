456acfcddc4250efcc5ec90a5f3a3b4c92cef72f
neginkheirmand <neginkheirmand@gmail.com>
2020-11-03 01:09:16 +0330

    last question done but there is a runtime error in 2 of the taste cases, will resolve it later

--- HW2/ciruelaLock.py
+++ HW2/ciruelaLock.py
@@ -2,6 +2,7 @@ def main():
     inputString = input().split()
     numLocks = int(inputString[0]) 
     lockNumbers = int(inputString[1]) 
+    # if numLocks==0 or lockNumbers==0:
     locks = []
     positionOfLocks = []
     for i in range(0, numLocks):
@@ -11,16 +12,17 @@ def main():
             newLock.append(int(inputString[j]))
         positionOfLocks.append(0)
         locks.append(newLock)
-    input()
-    redSolution = input().split()
-    for i in range(0, len(redSolution)):
-        redSolution[i]= int(redSolution[i])
-    repeatTimes = int(input())
+    num = input()
+    if num!=0:
+        redSolution = input().split()
+        for i in range(0, len(redSolution)):
+            redSolution[i]= int(redSolution[i])
+        repeatTimes = int(input())
 
-    for i in range(0, repeatTimes):
-        positionOfLocks[i%len(locks)]+=redSolution[i%len(redSolution)]
-    for i in range(0, numLocks):
-        positionOfLocks[i]= positionOfLocks[i]%lockNumbers
-        print(locks[i][positionOfLocks[i]], end='')
+        for i in range(0, repeatTimes):
+            positionOfLocks[i%len(locks)]+=redSolution[i%len(redSolution)]
+        for i in range(0, numLocks):
+            # and to count the other rotation we just add the repeatTimes  
+            positionOfLocks[(i+repeatTimes)%len(positionOfLocks)]= positionOfLocks[(i+repeatTimes)%len(positionOfLocks)]%lockNumbers
+            print(locks[(i+repeatTimes)%len(positionOfLocks)][positionOfLocks[(i+repeatTimes)%len(positionOfLocks)]], end='')
 main()
-# print(-1%5)
