7deb2b8a0a527d9ed8e30bf12d6ff96835970233
neginkheirmand <neginkheirmand@gmail.com>
2020-12-28 14:02:50 +0330

    working on the A* main loop

--- HW4/skiResort.py
+++ HW4/skiResort.py
@@ -9,37 +9,72 @@ m = 0
 output=[]
 
 class Node:
-    def __init__(self, r, c, h, g):
+    def __init__(self, r, c, rDestination, cDestination, g):
         self.r = r
         self.c = c
-        self.h = h
+        self.h = self.getHvalue(rDestination, cDestination, r, c)
         self.g = g
 
     def updateGValue(self, newg):
-        if self.g< newg:
+        if self.g > newg:
             self.g = newg
 
+    def getHvalue(self, rDestination, cDestination, r, c):
+        #the H value:  the estimated movement cost to move from that given square on the grid to the final destination.
+        #we can use the Pythagoras theorem to get the distance but i prefer this method 
+        return abs(rDestination-r)+abs(cDestination-c)
+
+    def getFvalue( self ):
+        return self.g + self.h
+
+def containsNode(r, c, openedlist, newg):
+    #this method's function is to tell if a node with passed r and c exists in the list, if so update its Gvalue(if necessary) 
+    #return true if exists and False if not
+    for i in range(0, len(openedlist)):
+        if Node(openedlist[i]).c == c and Node(openedlist[i]).r == r:
+            #the G value will be updated (if necessary)
+            Node(openedlist[i]).updateGValue(newg) 
+            return True
+    return False
+            
 
 
 def getGvalue():
     #the G value:  the movement cost to move from the starting point to a given square on the grid, following the path generated to get there.
-
-def getHvalue(rDestination, cDestination, r, c):
-    #the H value:  the estimated movement cost to move from that given square on the grid to the final destination.
-    #we can use the Pythagoras theorem to get the distance but i prefer this method 
-    return abs(rDestination-r)+abs(cDestination-c)
-
-    
-
-def getFvalue():
+    print("Gvalue")
 
 
-def shortestPath(r1, c1, r2, c2):
+def addNeighbours(openList, r, c, nodeG, rDestination, cDestination):
+    #upper neighbour( if existent and available and non-repetitive)
+    if r-1>=0 and map[r-1][c]!='X':
+        if not containsNode(r-1, c, openList, nodeG+1):
+            openList.append(Node(r-1, c, rDestination, cDestination, nodeG))
+
+def findBestNode(openList):
+    #this methods find the best candidate to next move and returns its index in the open list
+    min=Node(openList[0]).getFvalue()
+    hValue = Node(openList[0]).getHvalue()
+    index = 0
+    for i in range(1, len(openList)):
+        if min > Node(open[i]).getFvalue():
+            min = Node(open[i]).getFvalue()
+            hValue = Node(openList[i]).getHvalue()
+            index = i
+        elif min == Node(open[i]).getFvalue() and hValue > Node(open[i]).getHvalue :
+            hValue = Node(openList[i]).getHvalue()
+            index = i
+    return i
+
+def shortestPath(rStart, cStart, r2, c2):
     #this method finds the shortest path between point in row r1 and column c1 of the map and point in row r2 and column c2
     #and returns the distance of the shortest path available
     #we dont need to keep the parent of each node, just update the distances if needed
     openList = []
     closedList = []
+    startNode = Node(rStart, cStart, r2, c2)
+    openList.append(startNode)
+    while len(open)!=0:
+        indexBest = findBestNode(openList)
 
      
 
