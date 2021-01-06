a920e01e0c31aaceebf6028265b9cf9a462d9b5e
neginkheirmand <neginkheirmand@gmail.com>
2021-01-07 03:23:59 +0330

    base of the algorithm for q2 is working fine

--- HW4/FastPath.py
+++ HW4/FastPath.py
@@ -4,27 +4,80 @@ def main():
     n = input()
     n = int(n)
     for i in range(0, n):
-        time = 0
-        data = input().split()
-        N=0
-        S=0
-        E=0
-        W=0
+        #the (x, y) tuple
+        tupleState = (0,0)
+        #the neighbours list, [N, E, S, W)]
+        #if the value in the first house of list is 1 means the path from the house in the north of this house (x, y) has been used once
+        neighbours = [0,0,0,0]
+        data = input()
+        movesDic = {}
+        movesDic[tupleState] = neighbours
         for j in range(0, len(data)):
             if data[j]=='N':
-                N+=1
+                #we find the house and plus 1 to the north neighbour
+                edgesRepetition = movesDic[tupleState]
+                edgesRepetition[0]=edgesRepetition[0]+1
+                movesDic[tupleState]= edgesRepetition
+                #now to the new house
+                newtupleState=(tupleState[0], tupleState[1]+1)
+                tupleState = newtupleState
+                #it either already exists in the dictionary or we add it 
+                if newtupleState in movesDic:
+                    edgesRepetition = movesDic[newtupleState]
+                    edgesRepetition[2]=edgesRepetition[2]+1
+                    movesDic[newtupleState] = edgesRepetition
+                else:
+                    #add the new house to the dic
+                    movesDic[newtupleState]= [0,0,1,0]
             elif data[j]=='S':
-                S+=1
+                #we find the house and plus 1 to the south neighbour
+                edgesRepetition = movesDic[tupleState]
+                edgesRepetition[2]=edgesRepetition[2]+1
+                movesDic[tupleState]= edgesRepetition
+                #now to the new house
+                newtupleState=(tupleState[0], tupleState[1]-1)
+                tupleState = newtupleState
+                #it either already exists in the dictionary or we add it 
+                if newtupleState in movesDic:
+                    edgesRepetition = movesDic[newtupleState]
+                    edgesRepetition[0]=edgesRepetition[0]+1
+                    movesDic[newtupleState] = edgesRepetition
+                else:
+                    #add the new house to the dic
+                    movesDic[newtupleState]= [1,0,0,0]
             elif data[j]=='E':
-                E+=1
+                #we find the house and plus 1 to the east neighbour
+                edgesRepetition = movesDic[tupleState]
+                edgesRepetition[1]=edgesRepetition[1]+1
+                movesDic[tupleState]= edgesRepetition
+                #now to the new house
+                newtupleState=(tupleState[0]+1, tupleState[1])
+                tupleState = newtupleState
+                #it either already exists in the dictionary or we add it 
+                if newtupleState in movesDic:
+                    edgesRepetition = movesDic[newtupleState]
+                    edgesRepetition[3]=edgesRepetition[3]+1
+                    movesDic[newtupleState] = edgesRepetition
+                else:
+                    #add the new house to the dic
+                    movesDic[newtupleState]= [0,0,0,1]
             elif data[j]=='W':
-                W+=1
-        mapSki = []
-        row=[]
-        for x in range(0, E+W):
-            row.append(0)
-        for y in range(0, N+S):
-            mapSki.append(row.copy())
+                #we find the house and plus 1 to the west neighbour
+                edgesRepetition = movesDic[tupleState]
+                edgesRepetition[3]=edgesRepetition[3]+1
+                movesDic[tupleState]= edgesRepetition
+                #now to the new house
+                newtupleState=(tupleState[0]-1, tupleState[1])
+                tupleState = newtupleState
+                #it either already exists in the dictionary or we add it 
+                if newtupleState in movesDic:
+                    edgesRepetition = movesDic[newtupleState]
+                    edgesRepetition[1]=edgesRepetition[1]+1
+                    movesDic[newtupleState] = edgesRepetition
+                else:
+                    #add the new house to the dic
+                    movesDic[newtupleState]= [0,1,0,0]
+            print(movesDic)
 
 
 
